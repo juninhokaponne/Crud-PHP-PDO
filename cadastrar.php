08bedc802c514ca934e4f23a951ca737a76be85c
juninhokaponne <juninhokaponnemg@hotmail.com>
2021-05-30 19:52:50 -0300

    initial commit

---
+++ cadastrar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<h1>C, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cadastrar</h1>
+    <form name="cad-usuario" method="POST" action="">
+        <label>Nome: </label>
+        <input type="text" name="nome" id="nome" placeholder="Nome completo"> <br><br>
+
+        <label>E-mail: </label>
+        <input type="email" name="email" id="email" placeholder="Seu melhor email"> <br><br>
+
+        <input type="submit" value="Cadastrar" name="CadUsuario">
+    </form>
+</body>
+</html>
