a659fe4ccce7971844a59b72a8e87ecb2ffc7523
juninhokaponne <juninhokaponnemg@hotmail.com>
2021-05-30 20:04:18 -0300

    adicionado conexao.php

---
+++ conexao.php
@@ -0,0 +1,20 @@
+<?php 
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "crud";
+$port = 3306;
+
+// conexao com a porta
+$conn = new PDO("mysql:host=$host;port=$port;dbname=".$dbname, $user, $pass);
+
+// conexao sem a porta
+// $conn = new PDO("mysql:host=$host;dbname=".$dbname, $user, $pass);
+
+
+
+
+
+
+?>
